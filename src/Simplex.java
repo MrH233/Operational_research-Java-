45d20918b8825446ee1ee2660865562cd76c34b3
MrH233 <1658721519@qq.com>
2021-04-14 21:21:12 +0800

    Initial commit

---
+++ src/Simplex.java
@@ -0,0 +1,128 @@
+class Simplex {
+    // column is larger than raw.
+    int[] a;
+    double[][] element;
+    int[] xB;
+    double[] cB;
+    double[] cJ;
+    int[] unbase;
+    double[] b_test;
+    double[] x_test;
+    public Simplex(double[][] element, int raw, int column, double[] cJ){
+        xB = new int[raw];
+        cB = new double[raw];
+        unbase = new int[column-raw-1];
+        b_test = new double[raw];
+        x_test = new double[column];
+        this.element =element;
+        a = new int[] {raw, column};
+        this.cJ = cJ;
+        for(int i=0,j=column - raw; j<column; i++, j++){
+            xB[i] = j;
+            if(i<column-raw-1)
+                unbase[i] = i+1;
+        }
+        System.arraycopy(this.cJ, xB[0]-1, cB, 0, xB.length);
+    }
+    public void GaussTran(){
+        for(int i=0; i<a[0]; i++){
+            for(int j=0; j<a[1]; j++)
+                if(j!=xB[i]){
+                    element[i][j] /= element[i][xB[i]]==0 ? 1 : element[i][xB[i]];
+                }
+            element[i][xB[i]] /= element[i][xB[i]]==0 ? 1 : element[i][xB[i]];
+            if(element[i][xB[i]] == 0){
+                System.out.println("That LP question has no solution.");
+                System.exit(1);
+            }
+            for(int k=0; k<a[0]; k++){
+                if(k!=i){
+                    double ele = element[k][xB[i]];
+                    for(int l=0; l<a[1]; l++)
+                        element[k][l] -= element[i][l]*ele;
+                }
+            }
+        }
+    }
+    public boolean checkAndIter(){
+        // check[0] go in, check[1] go out.
+        int[] check = new int[] {0,0};
+        x_test = new double[a[1]];
+        double compare = 0;
+        double bi = 1000000;
+        // X-test
+        for (int i : unbase) {
+            x_test[i] = cJ[i - 1];
+            for (int j = 0; j < xB.length; j++)
+                x_test[i] -= cB[j] * element[j][i];
+            if (compare < x_test[i]) {
+                compare = x_test[i];
+                check[0] = compare == 0 ? 0 : i;
+            }
+        }
+        if(check[0] == 0)
+            return false;
+        // B-test (1000000 refers to minus or infinite bi/aik)
+        for(int i=0; i<xB.length; i++){
+            if(element[i][0]==0){
+                check[1] = xB[i];
+                break;
+            }
+            b_test[i] = element[i][0]/element[i][check[0]]>=0 ? element[i][0]/element[i][check[0]]:1000000;
+            if(b_test[i] < bi) {
+                bi = b_test[i];
+                check[1] = xB[i];
+            }
+        }
+        // refresh xB, unbase
+        for(int i=0; i<xB.length || i<unbase.length;i++){
+            if(i<xB.length)
+                xB[i] = xB[i]==check[1] ? check[0]:xB[i];
+            cB[i] = cJ[xB[i]-1];
+            if(i<unbase.length)
+                unbase[i] = unbase[i]==check[0] ? check[1]:unbase[i];
+        }return true;
+    }
+    public void show(int sign){
+        // show the whole info of the question
+        System.out.print("b \t\t\t");
+        for(int j=-1; j<a[0]+1;j++){
+            for(int i=0; i<a[1]; i++){
+                if(j == -1 && i<a[1]-1)
+                    System.out.printf("x%d\t\t\t", i + 1);
+                else if (j < a[0] && j > -1)
+                    System.out.printf("%-8.2f \t", element[j][i]);
+                else if (j==a[0] && i==0)
+                    System.out.printf("X-test\t\t");
+                else if (j==a[0] && i>0)
+                    System.out.printf("%-8.2f \t", x_test[i]);
+            }
+            if(j==-1)
+                System.out.print("B-test \t\t");
+            else if(j < a[0] && sign != 1)
+                System.out.printf("%.2f \t", b_test[j]);
+            else
+                System.out.print("\\\\ \t");
+            System.out.print("\n");
+        }
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+    }
+    public double[] calculate(){
+        // The best deal stored in result, and the last one of result is max z.
+        double[] result = new double[element[0].length+1];
+        while(true){
+            GaussTran();
+            if(!checkAndIter())
+                break;
+            show(0);
+        }show(1);
+        System.out.println("Best solution:");
+        for(int i=0; i<xB.length || i<element[0].length+1; i++) {
+            if (i < xB.length) {
+                System.out.printf("x%d : %.2f\n", xB[i], element[i][0]);
+                result[xB[i]] = element[i][0];
+                result[element[0].length] += cJ[xB[i] - 1] * element[i][0];
+            }
+        }return result;
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args){
+        Simplex a = new Simplex(new double[][] {{40, 2, 3, 1, 0, 0},{40, 3, 2, 0, 1, 0},{20, 1, 1, 0, 0, 1}}, 3, 6,new double[] {8, 10, 0, 0, 0});
+		Simplex b = new Simplex(new double[][] {{4, 4, 2, 2, -1, 0, 0},{20, 2, 4, 0, 0, 1, 0},{16, 4, 8, 2, 0, 0, 1}}, 3, 7,new double[] {2, 1, 1, 0, 0, 0});
+        double[] result1 = a.calculate();
+        System.out.printf("Best answer is: %.2f \n", result1[a.element[0].length]);
+		double[] result2 = b.calculate();
+		System.out.printf("Best answer is: %.2f \n", result2[b.element[0].length]);
+    }
+}
